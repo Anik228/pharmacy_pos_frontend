6395daffc032abb89bc1989a64dc80a11f84da75
Anik Dey <aniklal2020@gmail.com>
2025-02-10 18:37:31 +0600

    Updated

--- src/app/mother/mother.component.ts
+++ src/app/mother/mother.component.ts
@@ -4,10 +4,12 @@ import { SelectorComponent } from '../selector/selector.component';
 import { SelectorTwoComponent } from '../selector-two/selector-two.component';
 import { RightSideComponent } from '../right-side/right-side.component';
 import { LeftSideComponent } from '../left-side/left-side.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-mother',
-  imports: [RightSideComponent,LeftSideComponent],
+  imports: [RightSideComponent,LeftSideComponent,HeaderComponent,FooterComponent],
   templateUrl: './mother.component.html',
   styleUrl: './mother.component.css'
 })
